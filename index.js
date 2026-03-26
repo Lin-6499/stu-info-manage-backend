3db465826c652ea39446e851c89b3268978afe6a
Lin-6499 <lin_6499@qq.com>
2026-03-26 21:33:27 +0800

    fix cors

--- index.js
+++ index.js
@@ -8,6 +8,16 @@ import express from "express";
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
 
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    if (req.method === "OPTIONS") {
+        return res.sendStatus(200);
+    }
+    next();
+});
+
 app.use(express.json())
 app.use(express.static(join(__dirname,'src','access')));
 const PORT = process.env.PORT || 3000;
